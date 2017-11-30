164716e5612ed4b27b6382e3c63d4a974b3d7f7e
Jedrzej Kozerawski <jedrzej.kozerawski@muncasterconsulting.com>
2017-11-29 17:54:38 -0800

    minor fixes

--- training_pairs_generation.py
+++ training_pairs_generation.py
@@ -29,6 +29,7 @@ class pairs():
 	def create_pairs(self):
 		pairsList = []
 		for i in xrange(self.noOfCategories):
+			print "Class", i+1,"out of", self.noOfCategories
 			negativeCategoriesList = np.delete(np.arange(0,self.noOfCategories),i)
 			for k in xrange(self.noOfPositiveExamples):
 				imageA, imageB = self.get_random_two_images(self.images[i], self.images[i])
@@ -39,19 +40,32 @@ class pairs():
 				pairsList.append([imageA,imageB,"0"])
 		return pairsList
 
-	def split_list_into_two_textfiles(self, pairsList):
-		txtFileLeft = ""
-		txtFileRight = ""
+	def split_list_into_four_textfiles(self, pairsList):
+		trainFileLeft = ""
+		trainFileRight = ""
+		valFileLeft = ""
+		valFileRight = ""
 		random.shuffle(pairsList)	# shuffle list of pairs
 		for line in pairsList:
-			txtFileLeft+= line[0]+" "+line[2]+"\n"
-			txtFile2Right+= line[1]+" "+line[2]+"\n"
-		fLeft = open('./txtFileLeft.txt','w')
-		fLeft.write(txtFileLeft)
-		fLeft.close()
-		fRight = open('./txtFileRight.txt','w')
-		fRight.write(txtFileRight)
-		fRight.close()
+			# choose randomly if it's a val or train case (split 70/30):
+			if(random.random()>0.3):
+				trainFileLeft+= line[0]+" "+line[2]+"\n"
+				trainFile2Right+= line[1]+" "+line[2]+"\n"
+			else:
+				valFileLeft+= line[0]+" "+line[2]+"\n"
+				valFile2Right+= line[1]+" "+line[2]+"\n"
+		f = open('./train_left.txt','w')
+		f.write(trainFileLeft)
+		f.close()
+		f = open('./train_right.txt','w')
+		f.write(trainFileRight)
+		f.close()
+		f = open('./val_left.txt','w')
+		f.write(valFileLeft)
+		f.close()
+		f = open('./val_right.txt','w')
+		f.write(valFileRight)
+		f.close()
 		
 			
 		
