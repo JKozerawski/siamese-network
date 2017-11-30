8ce46af747f62a95c9abe1892c4feb6f637dcbfd
Jedrzej Kozerawski <jedrzej.kozerawski@muncasterconsulting.com>
2017-11-29 16:56:01 -0800

    initial commit

---
+++ training_pairs_generation.py
@@ -0,0 +1,12 @@
+from glob import glob
+
+class pairs():
+	def __init__(self):
+	
+
+#------------------------------------------------------------------
+def main():
+	DATA_ROOT = "/media/jedrzej/Seagate/DATA/"
+
+if __name__ == "__main__":
+    main()
