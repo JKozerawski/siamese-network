124ecc071e027ce51b0fdd5e148fac864190be69
Jedrzej Kozerawski <jedrzej.kozerawski@muncasterconsulting.com>
2017-12-09 14:09:40 -0800

    Minor fixes

--- model/train.sh
+++ model/train.sh
@@ -3,6 +3,5 @@ set -e
 TOOLS=/home/jedrzej/work/caffe/tools
 $TOOLS/caffe train \
 -gpu all \
--solver=./solver.prototxt \
--weights=./siamese_base.caffemodel 2>&1 | tee log/model.log
+-solver=./solver.prototxt 2>&1 | tee log/model.log
 
