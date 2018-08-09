6c34f4f2fe4af4a2bb26e974d9680600fc1f59bb
JKozerawski <jedrzej.kozerawski@gmail.com>
2018-08-09 12:15:41 -0700

    Created testing routine

--- test.py
+++ test.py
@@ -1,26 +1,61 @@
-from training_pairs_generator import pairs
+import argparse
+import caffe
+from caffe_feature_extractor import CaffeFeatureExtractor
 
-class tester():
-	def __init__(self):
-		print "Tester initialized"
-		
-	def get_testing_data(self, path, noOfExamples):
-		testPairsGenerator = pairs(path, noOfExamples, noOfExamples)
-		testPairsGenerator.get_all_images_in_lists()
-		pairsList = testPairsGenerator.create_pairs()
-		return pairsList
+def preprocess(image_path):
+	image = caffe.io.load(image_path)	# load the image
+	return np.asarray(extractionNet.transformer.preprocess("data",image)/255.)	# preprocess the image
 	
-	def testSiamese(self, testList):
-		
-	def testInceptionFeatures(self, testList):
+def run_siamese(net, imageL_path, imageR_path):
+	# read and preprocess images:
+	imageL = preprocess(imageL_path)
+	imageR = preprocess(imageR_path)
 	
+	# prepare network input:
+	network_input = np.zeros((1,6,224,224))
+	network_input[0,:3,...] = imageL
+	network_input[0,3:,...] = imageR
 
-
-#------------------------------------------------------------------
-def main():
-	siameseTester = tester()
-	testList = siameseTester.get_testing_data("/media/jedrzej/SAMSUNG/DATA/ILSVRC2012/TRAIN/", 50)
+	# run the network:
+	net.blobs['data'].reshape(1,6,224,224)	# reshape input to make sure it matches size of the batch (in this example batch of size 1)
+	similarity_score = net.forward(data = network_input))["prob"].copy()[0][0]	# run the network and extract "prob" layer output
+	return similarity_score
 
 
 if __name__ == "__main__":
-    main()
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--imageL',
+		type=str,
+		default="",
+		help="Path to the left image"
+		)
+	parser.add_argument(
+		'--imageR',
+		type=str,
+		default="",
+		help="Path to the right image"
+		)
+	parser.add_argument(
+		'--model_dir',
+		type=str,
+		default="./model/",
+		help="Directory where the caffe model files are"
+		)
+	parser.add_argument(
+		'--inception_model_dir',
+		type=str,
+		default="./model_inception/",
+		help="Directory where the caffe model files are for the Inception V1 network"
+		)
+
+	FLAGS = parser.parse_args()
+	extractionNet = CaffeFeatureExtractor(
+		model_path = FLAGS.inception_model_dir+"googlenet_deploy.prototxt",
+		pretrained_path = FLAGS.inception_model_dir+"bvlc_googlenet.caffemodel",
+		blob = "pool/7x7_s1",
+		crop_size = 224,
+		mean_values = [104.0, 117.0, 123.0]		
+		)
+	siameseNet = caffe.Net(FLAGS.model_dir+"deploy.prototxt", FLAGS.model_dir+"siamese_iter_10000.caffemodel", caffe.TEST)
+    	run_siamese(siameseNet, FLAGS.imageL, FLAGS.imageR)
